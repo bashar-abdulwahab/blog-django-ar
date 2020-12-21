d2b0af6bb256ede98af264a01a69d2f69335a28a
bsh1999 <hbashark99@gmail.com>
2020-12-21 13:01:08 +0700

    new commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import post
+"""
+{
+     python manage.py createsuperuser
+    user: bsh19
+    password: 717456393@#$bhs}
+"""
 # Register your models here.
+
+admin.site.register(post)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_list_or_404
 from django.http import HttpResponse
 
-
+from . models import post
 # Create your views here.
+
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'الصفحه الرئيسيه',
+        'posts': post.objects.all(),
+    }
+    return render(request, 'blog/index.html', context)
+
+def about(request):
+    
+    return render(request,'blog/about.html', {'title': 'من انا'}, )
+
+
+def post_detail(request,post_id):
+    Post = get_list_or_404(post, pk = post_id)
+    context = {
+        'title': 'الصفحه الرئيسيه',
+        'post': Post,
+        }
+    return render(request,'blog/post_detail.html',context)
